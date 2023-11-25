db2c5aa956d303a6f53b9c8cd5bb68b64d9b6532
pedroroblesduten <pedroroblesduten@gmail.com>
2023-11-25 18:18:55 -0300

    trainer

--- load_data.py
+++ load_data.py
@@ -1,3 +1,76 @@
 import pandas as pd
 import torch
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
 
+class LoadData:
+    def __init__(self, csv_path, batch_size, label_columns):
+        self.csv_path = csv_path
+        self.batch_size = batch_size
+        self.label_columns = label_columns
+        self.train_df, self.val_df, self.test_df = self.load_and_split_csv()
+
+    def load_and_split_csv(self):
+        data = pd.read_csv(self.csv_path)
+        data[self.label_columns] = data[self.label_columns].astype(int)
+        train_val_df, test_df = train_test_split(data, test_size=0.1)
+        train_df, val_df = train_test_split(train_val_df, test_size=0.1)
+        return train_df, val_df, test_df
+
+    def create_dataloaders(self):
+        print(f'* loading data from {self.csv_path} *')
+        train_set = CustomDataset(self.train_df, self.label_columns)
+        val_set = CustomDataset(self.val_df, self.label_columns)
+        test_set = CustomDataset(self.test_df, self.label_columns)
+
+        train_loader = DataLoader(train_set, batch_size=self.batch_size, shuffle=True)
+        val_loader = DataLoader(val_set, batch_size=self.batch_size, shuffle=False)
+        test_loader = DataLoader(test_set, batch_size=self.batch_size, shuffle=False)
+
+        return train_loader, val_loader, test_loader
+
+class CustomDataset(Dataset):
+    def __init__(self, dataframe, label_columns):
+        self.dataframe = dataframe
+        self.label_columns = label_columns
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        message = self.dataframe.loc[index, 'message']
+        label_df = self.dataframe[self.label_columns]
+        labels = torch.tensor(label_df.loc[index].values, dtype=torch.float)
+
+        return {
+            'message_id': index,
+            'text': message,
+            'labels': labels
+        }
+
+if __name__ == '__main__':
+    # UNIT TEST FOR THE DATALOADER
+
+    # Initialize LoadData with test parameters
+    csv_path = './data/classificacao.csv'  # Make sure this path points to a valid test CSV file
+    batch_size = 2
+    label_columns = ['sexism', 'body', 'racism', 'homophobia', 'neutral']
+    loader = LoadData(csv_path, batch_size, label_columns)
+
+    # Call create_dataloaders method
+    train_loader, val_loader, test_loader = loader.create_dataloaders()
+
+    # Perform assert checks
+    # Check if the returned objects are DataLoader instances
+    assert isinstance(train_loader, DataLoader), "Train loader is not an instance of DataLoader"
+    assert isinstance(val_loader, DataLoader), "Validation loader is not an instance of DataLoader"
+    assert isinstance(test_loader, DataLoader), "Test loader is not an instance of DataLoader"
+
+    # Check if a batch from the dataloader contains expected keys
+    for batch in train_loader:
+        assert 'message_id' in batch, "'message_id' not in batch"
+        assert 'text' in batch, "'text' not in batch"
+        assert 'labels' in batch, "'labels' not in batch"
+        break  # Test only the first batch
+
+    print("All tests passed successfully. Dataloader is correct!")
--- trainer.py
+++ trainer.py
@@ -1,1 +1,137 @@
 import torch
+import torch.nn as nn
+from load_data import LoadData
+import os
+from tqdm import tqdm
+import numpy as np
+from torchvision.utils import save_image
+from models.vqvae import VQVAE
+from models.transformer import Transformer
+from utils import loading_bar, plot_losses
+import math
+
+
+class TrainerTransformer():
+    def __init__(
+        self,
+        batch_size,
+        device,
+        epochs,
+        save_model_path,
+        save_losses_path,
+        save_weights_interval,
+        mask_schedule,
+        sos_token,
+        mask_token,
+        vqvae_config,
+        load_data_config,
+        transformer_config
+    ):
+        self.huggingface_model_name = huggingface_model_name
+        self.model_name = model_name
+        self.device = device
+        self.epochs = epochs
+        self.save_model_path = save_model_path
+        self.save_losses_path = save_losses_path
+
+        self.load_data_config = load_data_config
+        self.loader = LoadData(**self.load_data_config.__dict__)
+
+        self.best_val_loss = 1e9
+
+    def initialize_model(self, to_gpu=True):
+        model = BertForSequenceClassification.from_pretrained(self.huggingface_model_name, num_labels=self.num_labels)
+        if to_gpu:
+            model.to(self.device)
+        
+        return model
+
+    def save_models(self, model, model_name, epoch, val_loss, path):
+        if val_loss < self.best_val_loss:
+            name = f"BestVal_{model_name}"
+            save_path = os.path.join(path, name)
+            torch.save(model.state_dict(), save_path)
+
+        if epoch % self.save_weights_interval == 0:
+            name = f"epoch_{epoch}_" + model_name
+            save_path = os.path.join(path, name)
+            torch.save(model.state_dict(), save_path)
+
+    def process_text():
+
+        pass
+
+    def run(self):
+        print('----------------------------')
+        print('   STARTING BERT TRAINING   ')
+        print('----------------------------')
+
+        # Loading data
+        train_dataloader, val_dataloader, _ = self.loader.create_dataloaders()
+
+        # Init model
+        model = self.initialize_model()
+
+        # Training configurations
+        criterion = torch.nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.AdamW(transformer.parameters(), lr=1e-5)
+
+        print('- training transformer model -')
+
+        all_train_loss = []
+        all_val_loss = []
+
+        for epoch in range(self.epochs):
+            epoch_train_losses = []
+            epoch_val_losses = []
+            model.train()
+            with tqdm(
+                total=len(train_dataloader) + len(val_dataloader),
+                    desc=f"EPOCH {epoch+1} WITH DATASET PARTITION {dataset_partition + 1}",
+                ) as bar:
+                for batch in train_dataloader:
+
+                    text_tokens, labels, ids = self.process_input(batch)
+                    logits = model(text_tokens)
+
+                    # Descending the gradient and updating the linear layer
+                    optimizer.zero_grad()
+                    loss = criterion(logits, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                    # Saving the batch loss
+                    loss_numpy = loss.detach().cpu().numpy()
+                    epoch_train_loss.append(loss_numpy)
+                    bar.update(1)
+
+                model.eval()
+                with torch.no_grad():
+                    for batch in val_dataloader:
+                        text_tokens, labels, ids = self.process_input(batch)
+
+                        # Forward pass through the model
+                        logits = model(text_tokens)
+
+                        # Getting the loss
+                        loss = criterion(logits, labels)
+
+                        # Saving the batch loss
+                        loss_numpy = loss.detach().cpu().numpy()
+                        epoch_val_loss.append(loss_numpy)
+                        bar.update(1)
+
+            all_train_loss.append(np.mean(epoch_train_losses))
+            all_val_loss.append(np.mean(epoch_val_losses))
+
+            # Plotting the losses
+            plot_losses(all_train_loss, all_val_loss, self.save_losses_path, self.model_name)
+
+            # Saving model checkpoints
+            self.save_models(model,
+                             self.model_name,
+                             epoch,
+                             np.mean(epoch_val_losses),
+                             self.save_model_path,
+            )
+            print("-")
